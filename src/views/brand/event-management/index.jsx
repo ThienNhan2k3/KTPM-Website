c697ca59d2ba5f96ffdb9ae0bda063b1edc45eb7
PG47 <86549123+PG47@users.noreply.github.com>
2024-07-21 01:33:35 +0700

    UI in EventManagement()

--- src/views/brand/event-management/index.jsx
+++ src/views/brand/event-management/index.jsx
@@ -1,17 +1,21 @@
 import SearchIcon from "@assets/images/search-icon.png";
+import EditIcon from "@assets/images/edit-icon.png";
 import "./styles.css";
 
 export default function EventManagement() {
   return (
     <div className="col">
       <div className="row">
-        <div className="col input-group mx-2" style={{ width: "400px" }}>
+        <div className="col input-group mx-2">
           <input
             type="text"
             className="form-control"
             placeholder="Search"
             name="search"
-            style={{ width: "200px" }}
+            style={{ 
+              width: "500px" ,
+              height: "fit-content"
+            }}
           />
           <div className="input-group-btn">
             <button className="btn btn-default" type="submit">
@@ -23,7 +27,7 @@ export default function EventManagement() {
         <button className="col mx-2 lx-button">Lắc Xì</button>
         <button className="col mx-2 add-button">Thêm</button>
       </div>
-      <table className="table">
+      <table className="table table-bordered my-3">
         <thead>
           <tr>
             <th scope="col">ID</th>
@@ -37,21 +41,93 @@ export default function EventManagement() {
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Marathon</td>
+            <td>Donate</td>
+            <td>15/02/2024</td>
+            <td>18/02/2024</td>
+            <td>
+            <button
+                style={{ 
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "24px",
+                  height: "24px",
+                  padding: "0",  // Remove padding to ensure button size fits the image
+                  border: "none",  // Remove border if not needed
+                  //background: "none",  // Optional: Remove background if transparent button is desired
+                }}
+              >
+                <img
+                  src={EditIcon}
+                  alt="edit"
+                  style={{ 
+                    width: "20px",  // Adjust width of the image as needed
+                    height: "20px",  // Adjust height of the image as needed
+                  }}
+                />
+              </button>
+            </td>
           </tr>
           <tr>
             <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>Marathon2</td>
+            <td>Donate</td>
+            <td>15/02/2024</td>
+            <td>18/02/2024</td>
+            <td>
+            <button
+                style={{ 
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "24px",
+                  height: "24px",
+                  padding: "0",  // Remove padding to ensure button size fits the image
+                  border: "none",  // Remove border if not needed
+                  //background: "none",  // Optional: Remove background if transparent button is desired
+                }}
+              >
+                <img
+                  src={EditIcon}
+                  alt="edit"
+                  style={{ 
+                    width: "20px",  // Adjust width of the image as needed
+                    height: "20px",  // Adjust height of the image as needed
+                  }}
+                />
+              </button>
+            </td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
+            <td>Marathon3</td>
+            <td>Donate</td>
+            <td>15/02/2024</td>
+            <td>18/02/2024</td>
+            <td>
+            <button
+                style={{ 
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "24px",
+                  height: "24px",
+                  padding: "0",  // Remove padding to ensure button size fits the image
+                  border: "none",  // Remove border if not needed
+                  //background: "none",  // Optional: Remove background if transparent button is desired
+                }}
+              >
+                <img
+                  src={EditIcon}
+                  alt="edit"
+                  style={{ 
+                    width: "20px",  // Adjust width of the image as needed
+                    height: "20px",  // Adjust height of the image as needed
+                  }}
+                />
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
